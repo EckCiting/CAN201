3c4de6786f46d1a49f83979879ea9dee6bb7593a
EckCiting <EckCiting@outlook.com>
2020-12-12 21:32:17 +0800

    Forth Test Set ip as the parameters when the program starts add config.py to store cross-file global variables

--- test.py
+++ test.py
@@ -1,16 +1,12 @@
 import file_client
 import file_list_client
 from FileUtil import traverse_files
+import sys
+import config
 
 if __name__ == '__main__':
-    VMA="127.0.0.1"
-    path = "share"
-    # udp_file_client.udp_request_file("mysql.png",("192.168.0.19",12002)) # pass
-    # file_list_client.send_file_list("127.0.0.1",['test1','test2']) #pass
-
-    #with open("share/1/test.txt", "r") as file:
-        #print(file.read())
-    cache_file_list = traverse_files(path)
-    print(cache_file_list[0])
-    with open(cache_file_list[0], "r") as file:
-        print(file.read())
+    virtual_machines=sys.argv[2]
+    #VMB=sys.argv[3]
+    config.VMA, config.VMB = virtual_machines.split(',')
+    print("VM1: %s VM2: %s " % (config.VMA,config.VMB))
+   # print("VMB: ", VMB)
